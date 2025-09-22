52cee2547240460a0f184ec347aadf9c26b6dc31
Shaik Sufiyan <sssk6317@gmail.com>
2025-09-22 18:07:19 +0530

    Use axios for fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products.js";
 import "./HomePage.css";
@@ -13,11 +14,16 @@ export function HomePage() {
         response.json() → you change the reply into real data (JSON format).
         .then((data) => { console.log(data) }) → when the data is ready, you print it to the console. */}
 
-    fetch('http://localhost:3000/api/products')
-        .then((response) => {  
-            return response.json();
-        }).then((data) => {
-            console.log(data);
+    {/* fetch('http://localhost:3000/api/products')
+            .then((response) => {  
+                return response.json();
+            }).then((data) => {
+                console.log(data);
+            }); 
+    */} 
+    axios.get('http://localhost:3000/api/products')
+        .then((response) => {
+            console.log(response.data);
         });
 
     return (
