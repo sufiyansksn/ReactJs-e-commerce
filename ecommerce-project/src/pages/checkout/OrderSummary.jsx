6e40a2b5b428413a9e6aacf6023af981227d92b7
Shaik Sufiyan <sssk6317@gmail.com>
2025-10-03 08:05:02 +0530

    Create delete cart item

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,4 +1,5 @@
 import dayjs from "dayjs";
+import axios from 'axios';
 import { formatMoney } from '../../utils/money.js'
 import { DeliveryOptions } from './DeliveryOptions.jsx'
 
@@ -16,6 +17,11 @@ export function OrderSummary({cart, deliveryOptions, loadCart}) {
                 return deliveryOption.id === cartItem.deliveryOptionId; //So the delivery Option matches with what inside the cartitem will return True and that's gonna be save in selectDeliveryOptions.
                 }
             );
+
+            const deleteCatrItem = async () => {
+                await axios.delete(`/api/cart-items/${cartItem.productId}`);
+                await loadCart();
+            }
 
             return (
                 <div key={cartItem.productId} className="cart-item-container">
@@ -46,7 +52,9 @@ export function OrderSummary({cart, deliveryOptions, loadCart}) {
                         <span className="update-quantity-link link-primary">
                         Update
                         </span>
-                        <span className="delete-quantity-link link-primary">
+                        <span className="delete-quantity-link link-primary"
+                            onClick={deleteCatrItem}
+                        >
                         Delete
                         </span>
                     </div>
