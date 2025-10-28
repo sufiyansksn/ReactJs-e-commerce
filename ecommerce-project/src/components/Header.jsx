976322dfe6747d3622948aff15961006be7ce9d0
Sufiyan <164585680+sufiyansksn@users.noreply.github.com>
2025-10-28 11:13:01 +0530

    Remove commented Link component from Header
    Removed commented-out Link component in Header.

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -12,12 +12,12 @@ export function Header({ cart }) { {/* getting cart form props */}
         <>
             <div className="header">
                 <div className="left-section">
-                    {/*<Link to="/" className="header-link">
-                     <img className="logo"
+                    {/* <Link to="/" className="header-link">
+                    <img className="logo"
                         src="images/logo-white.png" />
                     <img className="mobile-logo"
                         src="images/mobile-logo-white.png" /> 
-                    </Link> */}
+                    </Link>  */}
                 </div> 
 
                 <div className="middle-section">
