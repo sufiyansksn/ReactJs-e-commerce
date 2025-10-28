0efa2cb6146d3fa3780946b7a88550d27f96114f
Sufiyan <164585680+sufiyansksn@users.noreply.github.com>
2025-10-28 10:42:36 +0530

    Update Header.jsx

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -13,8 +13,8 @@ export function Header({ cart }) { {/* getting cart form props */}
             <div className="header">
                 <div className="left-section">
                     <Link to="/" className="header-link">
-                    <img className="logo"
-                        src="images/logo-white.png" />
+                    {/* <img className="logo"
+                        src="images/logo-white.png" /> */}
                     <img className="mobile-logo"
                         src="images/mobile-logo-white.png" />
                     </Link>
